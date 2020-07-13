0425cd7c8c6be45b5a523c47ff181a1ca47dbb00
rusefi <rusefillc@gmail.com>
2020-07-12 21:48:05 -0400

    structure export from prod instance

---
+++ db/msqur-rusefi.sql
@@ -0,0 +1,75 @@
+--
+-- Table structure for table `msqur_engines`
+--
+
+CREATE TABLE `msqur_engines` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` tinytext,
+  `make` tinytext,
+  `code` tinytext,
+  `topic_id` int(11) DEFAULT NULL,
+  `displacement` decimal(4,2) NOT NULL,
+  `numCylinders` tinyint(2) DEFAULT NULL,
+  `compression` decimal(4,2) NOT NULL,
+  `induction` int(11) NOT NULL,
+  `injectorSize` int(11) DEFAULT NULL,
+  `twoStroke` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `injType` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nInjectors` tinyint(4) DEFAULT NULL,
+  `engineType` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msqur_files`
+--
+
+CREATE TABLE `msqur_files` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `crc` bigint(20) DEFAULT NULL,
+  `data` longblob,
+  `html` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msqur_logs`
+--
+
+CREATE TABLE `msqur_logs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `file_id` int(11) NOT NULL,
+  `tune_id` int(11) DEFAULT '0',
+  `info` mediumtext COLLATE utf8_unicode_ci,
+  `data` blob,
+  `views` int(10) NOT NULL DEFAULT '0',
+  `writeDate` datetime DEFAULT NULL,
+  `uploadDate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msqur_metadata`
+--
+
+CREATE TABLE `msqur_metadata` (
+  `id` int(11) NOT NULL,
+  `file` int(11) NOT NULL,
+  `engine` int(11) DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `views` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `fileFormat` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `firmware` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `writeDate` datetime DEFAULT NULL,
+  `uploadDate` datetime DEFAULT NULL,
+  `tuneComment` text,
+  `reingest` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
